97d797572a2b15d13c5ca1d5cf7d2a31856f1ab7
HussainFeroz <hussainferoz839@gmail.com>
2019-04-06 10:53:18 +0500

    Second Activity created and button listner added

--- app/src/main/java/com/example/mseassignments/MainActivity.java
+++ app/src/main/java/com/example/mseassignments/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.mseassignments;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button secondActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        secondActivity = (Button) findViewById(R.id.weatherActivity);
+        secondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent weatherIntent = new Intent(MainActivity.this, SecondActivity.class);
+
+                startActivity(weatherIntent);
+            }
+        });
     }
 }
