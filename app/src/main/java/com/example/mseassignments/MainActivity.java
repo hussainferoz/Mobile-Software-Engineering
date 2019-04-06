a2c9e304fede9d35f56b2b42b689b380f01ea758
HussainFeroz <hussainferoz839@gmail.com>
2019-04-06 11:26:53 +0500

    Layout Design of first activity

--- app/src/main/java/com/example/mseassignments/MainActivity.java
+++ app/src/main/java/com/example/mseassignments/MainActivity.java
@@ -8,7 +8,15 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private Button secondActivity;
+    private String humidity;
+    private String pressure;
+    private String windSpeed;
+    private String windDirection;
+    private String weather = "Clear";
+    private String minTemp;
+    private String maxTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +27,22 @@ public class MainActivity extends AppCompatActivity {
         secondActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                humidity = "60";
+                pressure = "971";
+                windSpeed = "53";
+                windDirection = "SE";
+                minTemp = "15";
+                maxTemp = "25";
 
                 Intent weatherIntent = new Intent(MainActivity.this, SecondActivity.class);
+
+                weatherIntent.putExtra("humidity", humidity);
+                weatherIntent.putExtra("pressure", pressure);
+                weatherIntent.putExtra("windSpeed", windSpeed);
+                weatherIntent.putExtra("windDirection", windDirection);
+                weatherIntent.putExtra("weather", weather);
+                weatherIntent.putExtra("minTemp", minTemp);
+                weatherIntent.putExtra("maxTemp", maxTemp);
 
                 startActivity(weatherIntent);
             }
--- app/src/main/java/com/example/mseassignments/SecondActivity.java
+++ app/src/main/java/com/example/mseassignments/SecondActivity.java
@@ -2,12 +2,41 @@ package com.example.mseassignments;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private TextView humidity;
+    private TextView pressure;
+    private TextView wind;
+    private TextView weather;
+    private TextView minTemp;
+    private TextView maxTemp;
+    private ImageView image;
+    private String condition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        humidity = (TextView) findViewById(R.id.humidityValue);
+        pressure = (TextView) findViewById(R.id.pressureValue);
+        wind = (TextView) findViewById(R.id.windValue);
+        weather = (TextView) findViewById(R.id.weather);
+        minTemp = (TextView) findViewById(R.id.min);
+        maxTemp = (TextView) findViewById(R.id.max);
+        image = (ImageView) findViewById(R.id.image);
+
+        humidity.setText(getIntent().getStringExtra("humidity") + " %");
+        pressure.setText(getIntent().getStringExtra("pressure") + " hpa");
+        wind.setText(getIntent().getStringExtra("windSpeed") + " km/h " + getIntent().getStringExtra("windDirection"));
+        //weather.setText(getIntent().getStringExtra("weather"));
+        minTemp.setText(getIntent().getStringExtra("minTemp") + "\u2103");
+        maxTemp.setText(getIntent().getStringExtra("maxTemp") + "\u2103");
+
+        condition = getIntent().getStringExtra("weather");
+        weather.setText(condition);
     }
 }
